a1f9b6114c059605059d3b9325f411693c2b5f86
Barry Hennessy <barryhenn@gmail.com>
2014-01-28 10:44:49 +0000

    Classifier: tabs begone

--- classifier.py
+++ classifier.py
@@ -7,40 +7,41 @@ from numpy import zeros, uint8, float16
 from classifier.rbm_classifier import RBMClassifier
 from sklearn import metrics
 from sklearn.cross_validation import train_test_split
-from sklearn import datasets
+
 
 @profile
 def foo():
-	input_parser = InputParser()
+    input_parser = InputParser()
 
-	target_pixels = zeros((42000, 784), float16)
-	target_numbers = zeros((42000, ), uint8)
+    target_pixels = zeros((42000, 784), float16)
+    target_numbers = zeros((42000, ), uint8)
 
-	sample_count = 0
-	for sample_data in input_parser.parse_train(sys.argv[1]):
-		pixels = uint8(sample_data[1])
+    sample_count = 0
+    for sample_data in input_parser.parse_train(sys.argv[1]):
+        pixels = uint8(sample_data[1])
 
-		target_numbers[sample_count] = sample_data[0]
-		target_pixels[sample_count] = pixels
+        target_numbers[sample_count] = sample_data[0]
+        target_pixels[sample_count] = pixels
 
-		sample_count += 1
+        sample_count += 1
 
+    # 0-1 scaling
+    target_pixels /= 255
 
-	# 0-1 scaling. Making presumptions about input data, ok for this test
-	target_pixels /= 255
+    training_set_pixels, testing_set_pixels, training_set_numbers, testing_set_numbers = train_test_split(
+        target_pixels, target_numbers, test_size = 0.3
+    )
 
-	training_set_pixels, testing_set_pixels, training_set_numbers, testing_set_numbers = train_test_split(
-		target_pixels, target_numbers, test_size = 0.3
-	)
-		
-	classifier = RBMClassifier()
-	classifier.train(training_set_numbers, training_set_pixels)
+    classifier = RBMClassifier()
 
-	print metrics.classification_report(
-		testing_set_numbers,
-		classifier.predict(testing_set_pixels)
-	)
+    classifier.train(training_set_numbers, training_set_pixels)
 
-	classifier.plot_rbm_features()
+    print metrics.classification_report(
+        testing_set_numbers,
+        classifier.predict(testing_set_pixels)
+    )
 
-foo()
+    classifier.plot_rbm_features()
+
+if __name__ == "__main__":
+    foo()
