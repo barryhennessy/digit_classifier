bde9423d016be6b145b41ac5c30a36ca39d3f9e2
Barry Hennessy <barryhenn@gmail.com>
2014-01-28 10:44:11 +0000

    Classifier: minor refactoring of training logic to split out grid search for parameters

--- classifier/rbm_classifier.py
+++ classifier/rbm_classifier.py
@@ -1,42 +1,63 @@
 from sklearn import linear_model
 from sklearn.neural_network import BernoulliRBM
+from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
 from math import sqrt, ceil
-import pdb
 
 class RBMClassifier(object):
     """Classifies images as integer 0-9 and tests given input
     """
 
-    # Choosing number of hidden components as 10 since we have 10 numbers to
-    # learn
-    num_components = 10
-
     classifier = None
 
-    def train(self, target_numbers, pixels):
-        rbm = BernoulliRBM(n_components = self.num_components)
+    def train(self, target_numbers, pixels, parameter_space=None):
+        rbm = BernoulliRBM()
+        rbm.n_components = 100
+        rbm.learning_rate = 0.01
+        rbm.n_iter = 10
 
-        # @TODO: Figure out and tune these
-        rbm.learning_rate = 0.06
-        rbm.n_iter = 20
-
-        logictic_regression = linear_model.LogisticRegression()
-
-        # @TODO: Figure out and tune these
-        # logictic_regression.C = 6000.0
+        logistic_regression = linear_model.LogisticRegression()
+        logistic_regression.C = 10000
 
         classification_steps = [
             ("rbm", rbm),
-            ("logistic", logictic_regression)
+            ("logistic", logistic_regression)
         ]
-        self.classifier = Pipeline(steps=classification_steps)
 
-        return self.classifier.fit(
+        pipeline = Pipeline(steps=classification_steps)
+
+        if parameter_space is not None:
+            self.classifier=self._train_with_grid_search(
+                pipeline,
+                parameter_space
+            )
+        else:
+            self.classifier=pipeline
+
+        self.classifier.fit(
             pixels,
             target_numbers
         )
+
+    def _train_with_grid_search(self, pipeline, parameter_space):
+        # @TODO: test this
+        return GridSearchCV(
+            pipeline,
+            param_grid=parameter_space,
+            verbose=2,
+            n_jobs=3,
+            pre_dispatch="10*n_jobs"
+        )
+
+    def print_grid_search_details(self, parameter_space):
+        # @TODO: test this
+        print("Best score: %0.3f" % self.classifier.best_score_)
+        print("Best parameters set:")
+        best_parameters = self.classifier.best_estimator_.get_params()
+        for param_name in sorted(parameter_space.keys()):
+            print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
 
     def predict(self, unclassified_image_pixels):
         if (self.classifier is None):
