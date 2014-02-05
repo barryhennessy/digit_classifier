5f5d6863168afdfbf28f0314fc013fd7203ec2f4
Barry Hennessy <barryhenn@gmail.com>
2014-02-05 16:54:02 +0000

    Docs: Documenting classifier.py

--- classifier.py
+++ classifier.py
@@ -1,38 +1,67 @@
 #! /usr/bin/python
 
+"""Training classifier
+
+This script is used to train a classifier on the `training_set` and predict
+it's classification accuracy on part of it.
+
+Usage:
+classifier.py /path/to/training/set.csv
+
+Where the format of the training set csv file is defined by the class formatted_io.TrainingSetIO
+
+Output displays classification accuracy per number and displays the misclassified images.
+Like so:
+
+                 precision    recall  f1-score   support
+
+              0       0.91      0.93      0.92       150
+              1       0.97      0.97      0.97       158
+              2       0.89      0.89      0.89       144
+              3       0.84      0.87      0.85       166
+              4       0.85      0.88      0.87       138
+              5       0.88      0.81      0.84       132
+              6       0.93      0.92      0.92       155
+              7       0.88      0.90      0.89       170
+              8       0.80      0.81      0.80       135
+              9       0.80      0.76      0.78       152
+
+    avg / total       0.88      0.88      0.88      1500
+
+    Displaying misclassified images.
+    Close to finish script.
+"""
+
 import sys
 from data_vis.image_display import ImageDisplay
 from formatted_io import TrainingSetIO
-from memory_profiler import profile
 from classifier.rbm_classifier import RBMClassifier
 from sklearn import metrics
 from sklearn.cross_validation import train_test_split
 
+training_parser = TrainingSetIO()
+target_numbers, target_pixels = training_parser.parse(sys.argv[1])
 
-@profile
-def foo():
-    training_parser = TrainingSetIO()
+training_set_pixels, testing_set_pixels, training_set_numbers, testing_set_numbers = train_test_split(
+    target_pixels, target_numbers, test_size = 0.3
+)
 
-    target_numbers, target_pixels = training_parser.parse(sys.argv[1])
+classifier = RBMClassifier()
+classifier.train(training_set_numbers, training_set_pixels)
 
-    training_set_pixels, testing_set_pixels, training_set_numbers, testing_set_numbers = train_test_split(
-        target_pixels, target_numbers, test_size = 0.3
-    )
+test_set_predicted_numbers = classifier.predict(testing_set_pixels)
 
-    classifier = RBMClassifier()
-    classifier.train(training_set_numbers, training_set_pixels)
+print metrics.classification_report(
+    testing_set_numbers,
+    test_set_predicted_numbers
+)
 
-    test_set_predicted_numbers = classifier.predict(testing_set_pixels)
+image_plotter = ImageDisplay()
+print("Displaying misclassified images...\nClose plot to finish script.")
+plot = image_plotter.plot_incorrect_classifications(
+    test_set_predicted_numbers,
+    testing_set_numbers,
+    testing_set_pixels
+)
+plot.show()
 
-    print metrics.classification_report(
-        testing_set_numbers,
-        test_set_predicted_numbers
-    )
-
-    image_plotter = ImageDisplay()
-    plot = image_plotter.plot_incorrect_classifications(test_set_predicted_numbers, testing_set_numbers, testing_set_pixels)
-    plot.show()
-    # classifier.plot_rbm_features()
-
-if __name__ == "__main__":
-    foo()
